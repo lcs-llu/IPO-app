2221b8d24e2d88aa0f2ab4a68cdc3a7a569e83c0
Leo Lu <llu@lcs.on.ca>
2021-12-11 09:46:57 -0500

    Initial Commit

---
+++ IPO app/IPO_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPO_appApp.swift
+//  IPO app
+//
+//  Created by Leo Lu on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct IPO_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
